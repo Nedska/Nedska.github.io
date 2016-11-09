de1d08e4c5ed59a5c4806462caccb7e7d7d5f9bb
Nedas <nedas0410@gmail.com>
2016-11-09 18:38:48 +0000

    CLEAN UP

---
+++ Backend/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype_Website.Backend
+{
+    class Class1
+    {
+        static int Main(string[] args)
+        {
+            //... 
+            return 0;
+        }
+    }
+}
