eb1f2809ee60ae89298cd7343fb627f39e5bf774
Nedas <nedas0410@gmail.com>
2016-11-11 17:46:19 +0000

    Small changes, clean up.

--- Backend/Class1.cs
+++ Backend/Class1.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace Prototype_Website.Backend
 {
+    SqlConnection myConnection = new SqlConnection("user id=username;" +
+                                       "password=password;server=serverurl;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=database; " +
+                                       "connection timeout=30");
+
     class Class1
     {
         static int Main(string[] args)
